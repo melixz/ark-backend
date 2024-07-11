a6aad001a0a53206398253ff6c44be0e8cc139e7
Vlad <dr.melix@gmail.com>
2024-07-11 22:05:14 +0300

    Добавил models.Manager() в свойство моделей

--- backend/main_page/models.py
+++ backend/main_page/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 
 class Header(models.Model):
-    left_image = models.ImageField(upload_to='images/')
+    objects = models.Manager(
+        left_image=models.ImageField(upload_to='images/'))
     left_alt = models.CharField(max_length=255)
     center_text = models.CharField(max_length=255)
     right_buttons = models.JSONField()
 
 
 class MainSection(models.Model):
-    title = models.CharField(max_length=255)
+    objects = models.Manager(
+        title=models.CharField(max_length=255))
     content = models.TextField()
 
 
 class FooterSection(models.Model):
-    title = models.CharField(max_length=255)
+    objects = models.Manager(
+        title=models.CharField(max_length=255))
     content = models.TextField()
