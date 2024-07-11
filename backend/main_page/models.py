e24f61c5587c89e8f60b3d9eb49fdae214e8d721
Vlad <dr.melix@gmail.com>
2024-07-11 22:54:26 +0300

    Создал кастомный менеджер в models.py и добавил drf_yasg

--- backend/main_page/models.py
+++ backend/main_page/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 
 
+class CustomManager(models.Manager):
+    def custom_method(self):
+        pass
+
+
 class Header(models.Model):
-    objects = models.Manager(
-        left_image=models.ImageField(upload_to='images/'))
+    left_image = models.ImageField(upload_to='images/')
     left_alt = models.CharField(max_length=255)
     center_text = models.CharField(max_length=255)
     right_buttons = models.JSONField()
+    objects = CustomManager()
 
 
 class MainSection(models.Model):
-    objects = models.Manager(
-        title=models.CharField(max_length=255))
+    title = models.CharField(max_length=255)
     content = models.TextField()
+    objects = CustomManager()
 
 
 class FooterSection(models.Model):
-    objects = models.Manager(
-        title=models.CharField(max_length=255))
+    title = models.CharField(max_length=255)
     content = models.TextField()
+    objects = CustomManager()
--- backend/main_page/urls.py
+++ backend/main_page/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import HeaderViewSet, MainSectionViewSet, FooterSectionViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'headers', HeaderViewSet)
 router.register(r'main-sections', MainSectionViewSet)
 router.register(r'footer-sections', FooterSectionViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
