85ea6117b125f543a9c1059589a0275245dc21e0
Vlad <dr.melix@gmail.com>
2024-08-15 14:49:34 +0300

    Метод get_contact_form

--- backend/new_buildings/serializers.py
+++ backend/new_buildings/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import NewBuilding
 from main_page.serializers import ContactFormSerializer
-
+from main_page.models import ContactForm
 
 class NewBuildingSerializer(serializers.ModelSerializer):
     bgr_image = serializers.SerializerMethodField()
     routes = serializers.SerializerMethodField()
-    contact_form = ContactFormSerializer()
+    contact_form = serializers.SerializerMethodField()  # Используем SerializerMethodField
 
     class Meta:
         model = NewBuilding
@@ -17,3 +17,8 @@ class NewBuildingSerializer(serializers.ModelSerializer):
 
     def get_routes(self, obj):
         return obj.routes
+
+    def get_contact_form(self, obj):
+        # Создаем или получаем экземпляр ContactForm (например, можно создать новый)
+        contact_form = ContactForm()  # Или можно получить существующую форму, если она одна
+        return ContactFormSerializer(contact_form).data
--- backend/plot_buildings/serializers.py
+++ backend/plot_buildings/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import PlotBuilding
 from main_page.serializers import ContactFormSerializer
-
+from main_page.models import ContactForm
 
 class PlotBuildingSerializer(serializers.ModelSerializer):
     bgr_image = serializers.SerializerMethodField()
     routes = serializers.SerializerMethodField()
-    contact_form = ContactFormSerializer()
+    contact_form = serializers.SerializerMethodField()  # Используем SerializerMethodField
 
     class Meta:
         model = PlotBuilding
@@ -17,3 +17,8 @@ class PlotBuildingSerializer(serializers.ModelSerializer):
 
     def get_routes(self, obj):
         return obj.routes
+
+    def get_contact_form(self, obj):
+        # Создаем или получаем экземпляр ContactForm (например, можно создать новый)
+        contact_form = ContactForm()  # Или можно получить существующую форму, если она одна
+        return ContactFormSerializer(contact_form).data
