c911d46e4a43282387bbc4ec4541cba3c0107bd4
Vlad <dr.melix@gmail.com>
2024-08-28 00:07:55 +0300

    Работа

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,26 +105,26 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "verceldb",
-#         "USER": "default",
-#         "PASSWORD": "bMUxSJ65kmYh",
-#         "HOST": "ep-curly-mouse-a2n5eg3c-pooler.eu-central-1.aws.neon.tech",
-#         "PORT": "5432",
-#         "OPTIONS": {
-#             "sslmode": "require",
-#         },
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "verceldb",
+        "USER": "default",
+        "PASSWORD": "bMUxSJ65kmYh",
+        "HOST": "ep-curly-mouse-a2n5eg3c-pooler.eu-central-1.aws.neon.tech",
+        "PORT": "5432",
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
