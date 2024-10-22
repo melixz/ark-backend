857b43ff8e0bdb8f1c25bacb43bb49b5da196077
Vlad <dr.melix@gmail.com>
2024-10-22 23:48:20 +0300

    Мелкая правка

--- backend/ark/views.py
+++ backend/ark/views.py
@@ -141,7 +141,7 @@ class DynamicFormSubmissionView(APIView):
             f"Имя: {crm_data['name']}\n"
             f"Телефон: {crm_data['phone']}\n"
             f"Почта: {crm_data['email']}\n"
-            f"Ссылка: {crm_data['url']}\n"
+            f"Смотрел: {crm_data['url']}\n"
         )
 
         logger.debug(f"Отправляем сообщение в Telegram: {telegram_message}")
